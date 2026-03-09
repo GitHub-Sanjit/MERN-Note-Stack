e779dfebadea2ce32d5023d75b36b6f974b4e45b
sanjit <sanjit.est14@gmail.com>
2026-03-09 23:44:47 +0600

    Nodemon and Setting up our Routes

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,22 @@ import express from "express";
 
 const app = express();
 
+app.get("/api/notes", (req, res) => {
+  res.send("You Got 20 Notes");
+});
+
+app.post("/api/notes", (req, res) => {
+  res.status(201).json({ message: "Note Created Successfully!" });
+});
+
+app.put("/api/notes/:id", (req, res) => {
+  res.status(200).json({ message: "Note Updated Successfully!" });
+});
+
+app.delete("/api/notes/:id", (req, res) => {
+  res.status(200).json({ message: "Note Deleted Successfully!" });
+});
+
 app.listen(5001, () => {
-    console.log("Server is running on port 5001");
-})
+  console.log("Server is running on port 5001");
+});
