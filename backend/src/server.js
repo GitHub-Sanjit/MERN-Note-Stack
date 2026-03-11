77f963f745a1122ca7c13a24be9d686e384b2182
sanjit <sanjit.est14@gmail.com>
2026-03-11 13:23:24 +0600

    some update with HomePage

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,8 +1,11 @@
 import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+
 import notesRoutes from "./routes/notesRoutes.js";
 import { connectDB } from "./config/db.js";
-import dotenv from "dotenv";
 import rateLimiter from "./middleware/reateLimiter.js";
+
 
 dotenv.config();
 
@@ -12,6 +15,7 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.use(rateLimiter);
 
 app.use("/api/notes", notesRoutes);
